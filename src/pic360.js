ef73c123bed6c67e5ca8bbed284da73c4bb2d065
Jinjiang <zhaojinjiang@me.com>
2017-06-23 15:29:41 +0800

    Supported touchmove & mousemove.

--- src/pic360.js
+++ src/pic360.js
@@ -1,72 +1,181 @@
-function Pic360 (src, el) {
-  const width = el.offsetWidth
-  const height = el.offsetHeight
-  const size = 1000
+
+function Pic360 (src, el, needCtrl = false) {
+  if (!src || !el || !el.nodeType === 1) {
+    return null
+  }
+  this.src = src
+  this.el = el
+  this.initCanvas()
+  if (needCtrl) {
+    this.initControl()
+  }
+}
+
+Pic360.prototype.initCanvas = function () {
+  const { el, src } = this
+  const { offsetWidth, offsetHeight } = el
 
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(75, offsetWidth / offsetHeight, 0.1, 1000)
 
   const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(width, height)
+  renderer.setSize(offsetWidth, offsetHeight)
 
   el.appendChild(renderer.domElement)
 
   const texture = new THREE.TextureLoader().load(src)
   const mesh = new THREE.Mesh(
-    new THREE.SphereGeometry(size, 80, 50),
+    new THREE.SphereGeometry(this.SIZE, 80, 50),
     new THREE.MeshBasicMaterial({ map: texture })
   )
   mesh.scale.x = -1
 
   scene.add(mesh)
 
-  function animate () {
-    requestAnimationFrame(animate.bind(this))
-    const phi = (90 - this.lat) * Math.PI / 180
-    const theta = this.lon * Math.PI / 180
-    const x = size * Math.sin(phi) * Math.cos(theta)
-    const y = size * Math.cos(phi)
-    const z = size * Math.sin(phi) * Math.sin(theta)
-    camera.lookAt(new THREE.Vector3(x, y, z))
-    renderer.render(scene, camera)
-  }
+  this.renderer = renderer
+  this.scene = scene
+  this.camera = camera
 
   this.lat = 0
   this.lon = 0
 
-  animate.call(this)
+  this.draw()
+}
+
+Pic360.prototype.draw = function () {
+  const { renderer, scene, camera } = this
+
+  const phi = (90 - this.lat) * Math.PI / 180
+  const theta = (180 + this.lon) * Math.PI / 180
+  const x = this.SIZE * Math.sin(phi) * Math.cos(theta)
+  const y = this.SIZE * Math.cos(phi)
+  const z = this.SIZE * Math.sin(phi) * Math.sin(theta)
+
+  camera.lookAt(new THREE.Vector3(x, y, z))
+  renderer.render(scene, camera)
+
+  requestAnimationFrame(this.draw.bind(this))
 }
 
 Pic360.prototype.initControl = function () {
-  addEventListener('keydown', this.keyDownListener.bind(this))
+  const { el } = this
+
+  // up/down/left/right
+  const keydown = this.keyDownListener.bind(this)
+  addEventListener('keydown', keydown)
+
+  // mouse move
+  const mousedown = this.mouseMoveListener.bind(this)
+  el.addEventListener('mousedown', mousedown)
+
+  // touch move
+  const touchstart = this.touchMoveListener.bind(this)
+  el.addEventListener('touchstart', touchstart)
+
+  // mouse wheel
+  const mousewheel = this.mouseWheelListener.bind(this)
+  el.addEventListener('mousewheel', mousewheel)
+
+  // touch move, zoom ...
+  this.listeners = { keydown, mousedown, mousewheel, touchstart }
+}
+
+Pic360.prototype.endControl = function () {
+  const { el, listeners } = this
+  const {
+    keydown,
+    mousedown,
+    mousemove,
+    mouseup,
+    touchstart,
+    touchmove,
+    touchend,
+    mousewheel
+  } = listeners
+  removeEventListener('keydown', keydown)
+  el.removeEventListener('mousedown', mousedown)
+  removeEventListener('mousemove', mousemove)
+  removeEventListener('mouseup', mouseup)
+  el.removeEventListener('touchstart', touchstart)
+  removeEventListener('touchmove', touchmove)
+  removeEventListener('touchend', touchend)
+  removeEventListener('touchcancel', touchend)
+  el.removeEventListener('mousewheel', mousewheel)
 }
 
 Pic360.prototype.keyDownListener = function (e) {
   switch (e.keyCode) {
      case 37: // left
-     this.left(3)
+     this.lon -= 3
      break
      case 39: // right
-     this.right(3)
+     this.lon += 3
      break
      case 38: // up
-     this.up(3)
+     this.lat += 3
      break
      case 40: // down
-     this.down(3)
+     this.lat -= 3
      break
   }
 }
 
-Pic360.prototype.up = function (offset) {
-  this.lat += offset
+Pic360.prototype.mouseMoveListener = function (e) {
+  const { listeners } = this
+
+  function move (e) {
+    this.lon -= e.movementX
+    this.lat += e.movementY
+    if (this.lat < -60) this.lat = -60
+    if (this.lat > 60) this.lat = 60
+  }
+
+  function end (e) {
+    removeEventListener('mousemove', listeners.mousemove)
+    removeEventListener('mouseup', listeners.mouseup)
+  }
+
+  removeEventListener('mousemove', listeners.mousemove)
+  removeEventListener('mouseup', listeners.mouseup)
+
+  listeners.mousemove = move.bind(this)
+  listeners.mouseup = end.bind(this)
+  addEventListener('mousemove', listeners.mousemove)
+  addEventListener('mouseup', listeners.mouseup)
 }
-Pic360.prototype.down = function (offset) {
-  this.lat -= offset
+
+Pic360.prototype.touchMoveListener = function (e) {
+  const { lon, lat, listeners } = this
+  const startX = e.touches[0].clientX
+  const startY = e.touches[0].clientY
+
+  function move (e) {
+    e.preventDefault()
+    const movementX = e.touches[0].clientX - startX
+    const movementY = e.touches[0].clientY - startY
+    this.lon = lon - movementX
+    this.lat = lat + movementY
+    if (this.lat < -60) this.lat = -60
+    if (this.lat > 60) this.lat = 60
+  }
+
+  function end (e) {
+    removeEventListener('touchmove', listeners.touchmove)
+    removeEventListener('touchend', listeners.touchend)
+    removeEventListener('touchcancel', listeners.touchend)
+  }
+
+  removeEventListener('touchmove', listeners.touchmove)
+  removeEventListener('touchend', listeners.touchend)
+  removeEventListener('touchcancel', listeners.touchend)
+
+  listeners.touchmove = move.bind(this)
+  listeners.touchend = end.bind(this)
+  addEventListener('touchmove', listeners.touchmove)
+  addEventListener('touchend', listeners.touchend)
+  addEventListener('touchcancel', listeners.touchend)
 }
-Pic360.prototype.left = function (offset) {
-  this.lon -= offset
-}
-Pic360.prototype.right = function (offset) {
-  this.lon += offset
-}
+
+Pic360.prototype.mouseWheelListener = function (e) {}
+
+Pic360.prototype.SIZE = 1000
